c5be9adaf346f9b701696ba7e600c57f4ed04d92
Omar Castro <ocastro24@yahoo.com>
2015-06-12 17:54:08 -0600

    Added mad libs homework
    [#96840514]

---
+++ methods_and_rucursion/mad_libs.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+
+puts "Give me an adjective:"
+adjective = gets.chomp
+puts "Give me another adjective:"
+adjective_2 = gets.chomp
+puts "Give me a noun:"
+noun = gets.chomp
+puts "Give me a plural noun:"
+plural_noun = gets.chomp
+puts "Give me a game:"
+game = gets.chomp
+puts "Give me another plural noun:"
+plural_noun_2 = gets.chomp
+puts "Give me a verb ending in 'ing':"
+verb = gets.chomp
+puts "Give me a another verb ending in 'ing':"
+verb_2 = gets.chomp
+puts "Give me another plural noun:"
+plural_noun_3 = gets.chomp
+puts "Give me another verb ending in 'ing':"
+verb_3 = gets.chomp
+puts "Give me another noun:"
+noun_2 = gets.chomp
+puts "Give me a plant:"
+plant = gets.chomp
+puts "Give me a part of the body:"
+body_part = gets.chomp
+puts "Give me a place:"
+place = gets.chomp
+puts "Give me another verb ending 'ing':"
+verb_4 = gets.chomp
+puts "Give me another adjective:"
+adjective_3 = gets.chomp
+puts "Give me a number:"
+number = gets.chomp
+puts "Gives me another plural noun:"
+plural_noun_4 = gets.chomp
+
+puts "Here is your madlib:"
+puts "A vacation is when you take a trip to some #{adjective} place with your #{adjective_2} family. Usually you go to
+some place that is near a/an #{noun} or up on a/an #{noun_2}. A good vacation place is one where you can ride
+#{plural_noun} or play #{game} or go hunting for #{plural_noun_2}. I like to spend my time #{verb} or #{verb_2}.
+When parents on a vacation, they spend their time eating three #{plural_noun_3} a day, and fathers play golf, and
+mothers sit around #{verb_3}. Last summer, my little brother fell in a/an #{noun_2} and got poison #{plant} all over his
+#{body_part}. My family is going to go to (the) #{place}, and I will practice #{verb_4}. Parents need vacations more
+than kids because parents are always very #{adjective_3} and have to work #{number} hours every day all year making
+enough #{plural_noun_4} to pay for the vacation."
