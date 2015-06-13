a7f18e11d41f9e4fa0d1556a0256150f245b931b
Omar Castro <ocastro24@yahoo.com>
2015-06-12 21:28:10 -0600

    Added Bottle of beer
    [#96932178]

---
+++ methods_and_rucursion/bottles_of_beer.rb
@@ -0,0 +1,10 @@
+#!./usr/bin/env ruby
+
+(1..99).to_a.reverse.each do |a_number|
+  puts a_number
+
+loop do
+  puts "99 bottles of beer on the wall, #{a_number} botlles of beer. Take one down pass it around, 98 bottles of beer on the wall."
+
+
+end
